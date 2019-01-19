8867a0aa094fd2d5c02f5c31c7e475fd7f4259e7
Tino Thamjarat <tino.thamjarat@gmail.com>
2019-01-19 22:33:16 +0700

    Use tty-config to help generate config file
    - Set filename and extension
    - Set lookup path
    - Use user input to add write into the config file

--- lib/github_rb/commands/config.rb
+++ lib/github_rb/commands/config.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../command'
+require 'tty-config'
 
 module GithubRb
   module Commands
@@ -8,11 +9,20 @@ module GithubRb
       def initialize(github_api_key, options)
         @github_api_key = github_api_key
         @options = options
+
+        @config = TTY::Config.new
+        @config.filename = 'github_rb'
+        @config.extname = '.yml'
+
+        # Specify the directory for TTY::Config to look for a config file.
+        @config.append_path Dir.pwd
       end
 
       def execute(input: $stdin, output: $stdout)
-        # Command logic goes here ...
-        output.puts "OK"
+        @config.read if @config.persisted?
+        @config.set('GITHUB_API_KEY', value: @github_api_key)
+        @config.write(force: true)
+        output.puts 'Config updated!'
       end
     end
   end
